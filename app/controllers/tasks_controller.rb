97fd867e0f27a1b05ad4f813c1cb169e69c82427
seiya.oura <u776339k@ecs.osaka-u.ac.jp>
2019-05-06 13:33:34 +0000

    re-final-post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,5 @@
 class TasksController < ApplicationController
   
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in, only: [:show, :new, :create, :edit, :update, :destroy]
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
 
@@ -51,10 +50,6 @@ class TasksController < ApplicationController
   end
   
   private
-  
-  def set_task
-    @task = Task.find(params[:id])
-  end
   
   def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
